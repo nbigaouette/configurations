f97d53972d8c7527cdf22f6febc1bb8cce42fef6
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-07-27 10:25:22 -0400

    Added max virtual memory limit

---
+++ max_mem.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Copyright 2010 Nicolas Bigaouette <nbigaouette @t gmail com>
+# This file is part of configurations.git.
+# http://github.com/nbigaouette/configurations
+#
+# configurations.git is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# configurations.git is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with configurations.git. If not, see <http://www.gnu.org/licenses/>.
+
+
+# Maximum virtual memory set to 1GB
+ulimit -v 1048576
