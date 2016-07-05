2f2af56e5c713e02dc7f73777c0fe3ed174511b2
Nicolas Bigaouette <nbigaouette@rogue-research.com>
2016-07-05 09:30:42 -0400

    Cleanup extra old stuff

--- aliases.sh
+++ aliases.sh
@@ -23,75 +23,17 @@ function less()
         src-hilite-lesspipe.sh ${@} | ${less_bin}
     fi
 }
-# Reset slurm nodes to idle
-alias slurm_reset_nodes="sudo scontrol update NodeName=node[2-16] State=idle"
-
-# VNC
-# https://wiki.archlinux.org/index.php/Tightvnc
-alias vnc_eee="dbus-launch vncserver -localhost -geometry 1024x600 :1"
 
 # Gentoo
 alias emerge_update_all="sudo emerge --ask --quiet --verbose --update --deep --newuse --tree --keep-going world"
 alias resource="source /etc/profile && source ~/.bashrc"
-
-function initrd_extract()
-{
-    if [[ "${@}" == "" ]]; then
-        echo "Usage: initrd_gunzip <filename>"
-        return
-    fi
-
-    filename=`basename $1`
-    dirname=${filename/.img.gz/}
-    mkdir -p ${dirname}
-    cd ${dirname}
-    if [[ "${1:0:1}" == "/" ]]; then
-        gunzip -cd    ${1} | sudo cpio -i --make-directories --preserve-modification-time --verbose
-    else
-        gunzip -cd ../${1} | sudo cpio -i --make-directories --preserve-modification-time --verbose
-    fi
-}
-
-# Create folder in /mnt/data/nicolas/md/shockwave/`now`_NAME and link "output" to in
-function lnso() {
-    mnt="/mnt/data/nicolas/md/shockwave"
-    if [[ "${@}" == "" ]]; then
-        echo "Usage: lnso <name>"
-        return
-    fi
-    new_folder="${mnt}/`now`_${@}"
-
-    if [[ -h "output" ]]; then
-        cmd="rm -f output"
-        echo ${cmd}
-        ${cmd}
-    elif [[ -d "output" ]]; then
-        echo "ERROR: 'output' is a directory. Not deleting."
-        return
-    fi
-
-    cmd="mkdir -p ${new_folder}"
-    echo ${cmd}
-    ${cmd}
-
-    cmd="ln -s ${new_folder} output"
-    echo ${cmd}
-    ${cmd}
-
-    # Make sure output contains .gitignore and .kdev_ignore
-    echo "*" > output/.gitignore
-    touch output/.kdev_ignore
-}
 
 # Call "tail -f" on a file and put timestamp at the begining of each lines.
 function tailtime() {
     tail -f ${1} | awk '{now=strftime("%F %T%z\t");sub(/^/, now);print}'
 }
 
-alias squeue='squeue --format="%.7i %.9P %.100j %.8u %.8T %.10M %.9l %.6D %R"'
 alias locate='locate -i'
-alias aurbuild="sudo env EDITOR=nano aurbuild"
-alias burn_dvd_iso='growisofs -speed=2 -dvd-compat -Z /dev/dvdrw='
 #alias grep='grep -n'
 alias nano='nano -w'
 #alias screen="screen -U -R"
@@ -103,30 +45,15 @@ alias top="htop"
 alias untar="tar -xvf"
 #alias untar="tar -zxvf"
 #alias untarj="tar -jxvf"
-alias gmplayer="gmplayer -really-quiet"
-alias mp="mousepad"
 alias pingg="ping -c 3 www.google.com"
-alias kw='kwrite'
-#alias ku='kate -u'
-alias ku='/home/nicolas/fichiers/scripts/open_file_in_active_kate_session.sh'
-alias ki='kile '
-alias kwp='kate --start PKGBUILDs --new PKGBUILD &'
-#alias kwp='kwrite PKGBUILD &'
 alias octave='octave --quiet'
 alias ipython='ipython --pylab'
 alias ipython2='ipython2 --pylab'
 function pss() {
-    ps aux | grep -i "$@" | grep -v grep
+	ps auxf | grep -i "$@" | grep -v grep | grep --color=auto -i "$@"
 }
 alias pstree="ps axjf | less"
 alias scpr='rsync --partial --progress'
-
-alias arch32='schroot -c Arch32'
-alias arch64='schroot -c Arch64'
-alias makepkg32='dchroot -d makepkg'
-alias to64="sed -e \"s/arch=.*/arch=('i686' 'x86_64')/g\" -i PKGBUILD"
-alias c32="schroot -p -c chroot32"
-alias c64="schroot -p -c chroot64"
 
 alias dc='cd'
 alias sl='ls'
@@ -137,70 +64,34 @@ lsbase='/bin/ls -h --group-directories-first --color=auto'
 alias ls="$lsbase -l"
 alias la="$lsbase -la"
 
-fichiers=/home/nicolas/fichiers
-doc=$fichiers/documents/universite/doc
-PKGBUILDs=$fichiers/programmes/PKGBUILDs
-alias cdh='cd $fichiers'
-alias cdt='cd $fichiers/downloads'
-alias cdd='cd $doc'
-alias cdm='cd $doc/our_papers/mapping.git'
-alias cddn='cd $doc/notes'
-alias cdmd='cd $doc/code/projects/md.git'
-alias cdpr='cd $doc/code/projects/PIC3D.svn'
-alias cdpi='cd $doc/code/projects/ionization.svn'
-alias cdpt='cd $doc/code/projects/treecode.svn'
-alias cdpq='cd $doc/code/projects/quantumfdtd.git'
-alias cdp='cd $doc/code/projects'
-alias cdpk='cd $fichiers/programmes/PKGBUILDs'
+alias cdt='cd ~/Downloads'
 
 
 # Sudo's
 # Command line apps
-alias sudo='sudo -E'
+#alias sudo='sudo -E'
 #alias su='sudo -i'
 #alias su='sudo /bin/bash'
 alias su='sudo su -'
-alias shutdown="sudo shutdown"
-alias halt="sudo halt"
-alias nanos='sudo nano'
 alias snano='sudo nano'
 alias svim='sudo vim'
-alias cpufreq-set='sudo cpufreq-set'
-alias pac='pacman -Syu'
-alias pacman='sudo -E pacman'
-alias modprobe='sudo modprobe'
-alias telinit='sudo telinit'
-alias mount='sudo mount'
-alias umount='sudo umount'
 alias powertop='sudo powertop'
-
-alias yaourt='yaourt --export /home/nicolas/fichiers/programmes/ArchLinux/pkg_cache'
 
 
 ############################
 # Functions
 
 function aur() {
-    cd $fichiers/programmes/PKGBUILDs/aur
-    url=${1}
-    tarball=${url##http*/}
-    wget ${url}
-    untar ${tarball}
-    rm -f ${tarball}
-    cd ${tarball/.tar.gz/}
+    cd ${HOME}/aur
+    package=${1}
+    git clone https://aur.archlinux.org/${package}.git
+    cd ${package}
     echo "--------------------------"
     echo "Start 'makepkg'? [Y/n]"
     read -s answer
     if [[ "$answer" == "" || "$answer" == "Y" || "$answer" == "y" || "$answer" == "yes" || "$answer" == "oui" || "$answer" == "o" ]]; then
         makepkg
     fi
-}
-
-biggest() {
-	if [[ $1 -gt $2 ]]; then
-		return $1
-	else return $2
-	fi
 }
 
 killp () {
@@ -214,12 +105,8 @@ function ssi() {
     grep -rn "$@" src/*  | sed "s|\(.*\):\([0-9]\+\):\(.*\)|\1  Line \2  \3|g"
 }
 
-alias tosingle='sed -e "s|.*CFLAGS.*+=.*-DFLOATTYPE_SINGLE|CFLAGS          += -DFLOATTYPE_SINGLE|g" -i Makefile'
-alias todouble='sed -e "s|.*CFLAGS.*+=.*-DFLOATTYPE_SINGLE|#CFLAGS          += -DFLOATTYPE_SINGLE|g" -i Makefile'
-
-#alias gl='git log --all --pretty=oneline --graph'
 alias gl='git log --pretty=format:"%Cgreen%h %Creset %s %Cblueby %an (%ar) %Cred %d" --graph'
-alias gla='git log --all --pretty=format:"%Cgreen%h %Creset %s %Cblueby %an (%ar) %Cred %d" --graph'
+alias gla='git log --all --pretty=format:"%Cgreen%h %Creset %Cred%d%Creset %s %Cblueby %an (%ar)" --graph'
 #alias gd='git diff $* | kompare - &'
 alias gg='gitg 2> /dev/null &'
 alias gu='git gui &'
@@ -242,53 +129,21 @@ alias gd='git diff'
 alias gdc='git diff --cached'
 alias gm='git commit -m'
 alias gma='git commit -am'
-alias gb='git branch'
+alias gb='git branch --all --verbose'
 alias gc='git checkout'
 alias gra='git remote add'
 alias grr='git remote rm'
 alias gpu='git pull'
 alias gcl='git clone'
 
-alias git_clone_qf='git clone ssh://op/git/nicolas/quantumfdtd.git $1'
-alias git_clone_md='git clone ssh://op/git/nicolas/md.git $1'
-
-alias git_sharcnet='git archive --format=tar HEAD | ssh -C sn "cat > /home/nbiga058/code/${gitCODE}/${gitCODE}.tar && cd /home/nbiga058/code/${gitCODE} && tar -xvf /home/nbiga058/code/${gitCODE}/${gitCODE}.tar"'
-alias git_xenakis='git archive --format=tar HEAD | ssh -C xenakis "cat > /home/hpc1994/code/${gitCODE}/${gitCODE}.tar && cd /home/hpc1994/code/${gitCODE} && tar -xvf /home/hpc1994/code/${gitCODE}/${gitCODE}.tar"'
-alias git_hpcvl='git archive --format=tar HEAD | ssh -C hpcvl "cat > /home/hpc1994/code/${gitCODE}/${gitCODE}.tar && cd /home/hpc1994/code/${gitCODE} && tar -xvf /home/hpc1994/code/${gitCODE}/${gitCODE}.tar"'
-alias git_lonestar='git archive --format=tar HEAD | ssh -C lonestar "cat > /home/01184/lramunno/code/${gitCODE}/${gitCODE}.tar && cd /home/01184/lramunno/code/${gitCODE} && tar -xvf /home/01184/lramunno/code/${gitCODE}/${gitCODE}.tar"'
-alias git_discovery='git archive --format=tar HEAD | ssh -C discovery "cat > /home/lramunno/code/${gitCODE}/${gitCODE}.tar && cd /home/lramunno/code/${gitCODE} && tar -xvf /home/lramunno/code/${gitCODE}/${gitCODE}.tar"'
-
-function grb_track() {
-    RemoteBranch="${@}"
-    git fetch origin
-    git branch --track $RemoteBranch origin/$RemoteBranch
-    git checkout $RemoteBranch
-    git branch -a
-}
-
-function grb_create() {
-    RemoteBranch="${@}"
-    git push origin implicit:refs/heads/$RemoteBranch
-    git fetch origin
-    git branch --track $RemoteBranch origin/$RemoteBranch
-    git checkout $RemoteBranch
-    git branch -a
-}
-
-function rsync_unicron() {
-    # rsync --progress -aze ssh u:/raid/nicolas/output/md_production_Ar/ output/
-    if [[ "x${1}" == "x" ]]; then
-        echo "Usage:"
-        echo "rsync_unicron /full/path/on/unicron/ local/folder/"
-        echo "Will transfer everything using rsync"
-        return
-    fi
-    rsync --progress -aze ssh u:${1} ${2}
-}
 
 function check_memory() {
     while [ 1 ]; do pmap -d `pidof $1` | grep writeable | sed "s|K||g" | awk '{print ""$4" KiB    "$4/1024" MiB    "$4/1024/1024" GiB"}' ; sleep 0.1 ;done 2> /dev/null
 }
 alias cm=check_memory
 
-alias ssync='while [ 1 ]; do echo -n "Syncing..." && date && sync && sleep 3; done'
+
+alias qmlscene55lgpl="~/Qt/LGPL/5.5/gcc_64/bin/qmlscene"
+alias qmlscene56lgpl="~/Qt/LGPL/5.6/gcc_64/bin/qmlscene"
+alias qmlscene551debug="~/opt/Qt/5.5.1/debug/bin/qmlscene"
+alias qmlscene551release="~/opt/Qt/5.5.1/release/bin/qmlscene"
