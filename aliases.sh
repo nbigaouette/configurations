a7eaa80cbe08dcdf8af74aba06bd8ca24ce08982
Nicolas Bigaouette <nbigaouette@rogue-research.com>
2016-07-05 09:32:03 -0400

    Function fa() to find and open in atom

--- aliases.sh
+++ aliases.sh
@@ -5,6 +5,10 @@ alias now="date +%Y%m%d_%Hh%M"
 alias a="atom ."
 alias aa="atom -a ."
 
+function fa() {
+	find . -name ${1} -exec atom {} \;
+}
+
 alias uofo="while [[ 0 ]]; do sleep 5 && ssh uofo; done"
 alias primus="while [[ 0 ]]; do sleep 5 && ssh primus; done"
 
